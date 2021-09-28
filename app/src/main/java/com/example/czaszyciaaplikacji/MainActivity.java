ae894ea531fad06967ccbc05e8d11e18772dab69
markup54 <marzenakupka@tlen.pl>
2021-09-28 08:08:53 +0200

    widok poczatkowy

---
+++ app/src/main/java/com/example/czaszyciaaplikacji/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.czaszyciaaplikacji;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void wgore(View view) {
+    }
+
+    public void wdol(View view) {
+    }
+}
